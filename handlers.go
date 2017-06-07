d9544d01c7ef9e00c9bebc0f5950daf02a301774
Valeriy Kostikov <mkoctuk@gmail.com>
2017-06-08 00:13:11 +0300

    move handlers

---
+++ handlers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"path"
+	"path/filepath"
+
+	"github.com/goware/cors"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join("public", "index.html"))
+}
+
+func serveFileHandler(w http.ResponseWriter, r *http.Request) {
+	fname := path.Base(r.URL.Path)
+	http.ServeFile(w, r, filepath.Join("public", fname))
+}
+
+func corsHandler() *cors.Cors {
+	newCors := cors.New(cors.Options{
+		// AllowedOrigins:   []string{"*"},
+		AllowedOrigins: []string{
+			"http://localhost:8080",
+		},
+		AllowedMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowedHeaders: []string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+			"X-CSRF-Token",
+		},
+		ExposedHeaders: []string{
+			"Link",
+		},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	return newCors
+}
