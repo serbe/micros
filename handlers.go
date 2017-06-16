b972576e661ad008aa1760633ebf298f3e3abe8a
Valeriy Kostikov <serbenv@gmail.com>
2017-06-16 15:07:18 +0300

    add missing methods to cors

--- handlers.go
+++ handlers.go
@@ -18,7 +18,7 @@ func serveFileHandler(w http.ResponseWriter, r *http.Request) {
 func corsHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
