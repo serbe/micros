8ab084aa7a6ef4ece7ba70ffb6637ea1de32f05c
Valeriy Kostikov <serbenv@gmail.com>
2017-06-19 10:41:12 +0300

    update crud

--- handlers.go
+++ handlers.go
@@ -17,7 +17,7 @@ func serveFileHandler(w http.ResponseWriter, r *http.Request) {
 
 func corsHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080, http://cidev.ru:9090, http://localhost:9090")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
