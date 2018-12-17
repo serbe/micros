e38acd241443a4e15de326e0aa7df10932e7d766
Serbe <mkoctuk@gmail.com>
2018-12-17 11:10:40 +0300

    update dist

--- dist/js/NotFoundComponent.bfe20a82.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["NotFoundComponent"],{2946:function(n,o,t){"use strict";t.r(o);var e=function(){var n=this,o=n.$createElement,t=n._self._c||o;return t("div",[t("h1",[n._v("404 page not found")]),t("router-link",{attrs:{to:"/"}},[n._v("Home")])],1)},u=[],l=(t("cadf"),t("551c"),t("097d"),{}),r=l,a=t("2877"),c=Object(a["a"])(r,e,u,!1,null,null,null);c.options.__file="NotFoundComponent.vue";o["default"]=c.exports}}]);
-//# sourceMappingURL=NotFoundComponent.bfe20a82.js.map
