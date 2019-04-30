40b71ee923ba3b0bdb9e6785e470897b188f6bb5
Serbe <mkoctuk@gmail.com>
2019-04-30 10:59:16 +0300

    update dist

---
+++ dist/js/NotFoundComponent.97a4ef8e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["NotFoundComponent"],{2946:function(n,t,e){"use strict";e.r(t);var o=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("h1",[n._v("404 page not found")]),e("router-link",{attrs:{to:"/"}},[n._v("Home")])],1)},u=[],r={},l=r,a=e("2877"),s=Object(a["a"])(l,o,u,!1,null,null,null);t["default"]=s.exports}}]);
+//# sourceMappingURL=NotFoundComponent.97a4ef8e.js.map
